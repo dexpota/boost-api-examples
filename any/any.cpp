ff8caadf065c6cab67e263ef811c710763f09b90
Fabrizio Destro <destro.fabrizio@gmail.com>
2017-05-26 18:04:09 +0200

    'Any' example updated.

--- any/any.cpp
+++ any/any.cpp
@@ -2,16 +2,31 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cassert>
+
 
 int main(int argc, char** argv) {
-	std::vector<boost::any> some_values;
-	
-	some_values.push_back(10);
-	const char* c_str = "Hello there!";
-	some_values.push_back(c_str);
-	some_values.push_back(std::string("Wow!"));
-	std::string& s = boost::any_cast<std::string&>(some_values.back());
-	s += " That is great!\n";
-	std::cout << s;
+
+	{
+		boost::any any_value = 10;
+		std::cout << boost::any_cast<int>(any_value) << std::endl;
+		int *pointer = boost::any_cast<int>(&any_value);
+		std::cout << *pointer << std::endl;
+
+		any_value = 2.34;
+		std::cout << boost::any_cast<double>(any_value) << std::endl;
+
+		any_value = false;
+		std::cout << std::boolalpha << boost::any_cast<bool>(any_value) << std::endl;
+	}
+
+	{
+		boost::any any_value = 10;
+		assert(any_value.type() == typeid(int));
+
+		boost::any empty_value;
+		assert(empty_value.empty());
+	}
+
 	return 0;
 }
