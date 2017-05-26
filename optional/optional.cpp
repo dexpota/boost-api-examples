2c7e15889e46f9c6fda6d275f01010fbd148a87b
Fabrizio Destro <destro.fabrizio@gmail.com>
2017-05-26 22:30:55 +0200

    optional value

--- optional/optional.cpp
+++ optional/optional.cpp
@@ -2,37 +2,32 @@
 #include <iostream>
 #include <stdlib.h>
 
-class locked_device {
-	explicit locked_device(const char *param) {
-		std::cout << "Device is locked.\n";
-	}
-public:
-	~locked_device() {}
-	
-	void use() {
-		std::cout << "Success!\n";
-	}
-	
-	static boost::optional<locked_device> try_lock_device() {
-		if(rand()%2) {
-			return boost::none;
-		}
-		return locked_device("device name");
-	}
-};
+boost::optional<int> return_value() {
+    return boost::optional<int>(10);
+}
+
+boost::optional<int> return_none() {
+    return boost::none;
+}
 
 int main(int argc, char **argv){
-	srandom(5);
-	for(unsigned i=0; i<10; ++i) {
-		boost::optional<locked_device> t = locked_device::try_lock_device();
-		
-		if(t) {
-			t->use();
-			return 0;
-		}else {
-			std::cout << "...trying again.\n";
-		}
-	}
-	std::cout << "Failure!\n";
-	return -1;
+    // none value converted to bool is false
+    assert(!return_none());
+
+    // return 10 because the optional value is none
+    assert(return_none().value_or(10) == 10);
+
+    // call the function because the optional value is none
+    assert(return_none().value_or_eval([](){return 10;}) == 10);
+
+    try {
+        return_none().value();
+    }catch (const boost::bad_optional_access&) {
+        std::cout << "It's none." << std::endl;
+    }
+
+    // return an optional filled with a value
+    assert(return_value().value() == 10);
+
+    return EXIT_SUCCESS;
 }
