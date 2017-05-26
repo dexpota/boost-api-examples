32b4ebc0c4b99b8e20423af68695ef8727b2fa4c
Fabrizio Destro <destro.fabrizio@gmail.com>
2017-05-26 21:50:37 +0200

    Code cleaning.

--- array/array.cpp
+++ array/array.cpp
@@ -1,5 +1,5 @@
 #include <boost/array.hpp>
-#include <algorithm>
+#include <iostream>
 #include <array>
 
 struct add_1 : public std::unary_function<char, void> {
@@ -9,16 +9,10 @@ struct add_1 : public std::unary_function<char, void> {
 };
 
 
-typedef boost::array<char, 4> array4_t;
-array4_t& vector_advance(array4_t& val) {
-	std::for_each(val.begin(), val.end(), add_1());
-	return val;
-}
-
 int main(int argc, char **argv) {
-	array4_t val = {0,1,2,3};
-	// std::array<int,4> p;
-	array4_t val_res;
-	val_res = vector_advance(val);
-	return 0;
+	// std::array is now part of the C++11 standard
+	boost::array<int, 5> values = {0, 1, 2, 3, 5};
+
+    std::copy(values.begin(), values.end(), std::ostream_iterator<int>(std::cout, " "));
+	return EXIT_SUCCESS;
 }
--- bind/bind.cpp
+++ bind/bind.cpp
@@ -1,8 +1,10 @@
 #include <boost/bind.hpp>
+#include <boost/array.hpp>
 #include <functional>
 #include <vector>
 #include <iostream>
 #include <cmath>
+
 
 void print_number(std::string prefix, int number) {
 	std::cout << prefix << number << std::endl;
@@ -22,15 +24,22 @@ struct Foo {
 	}
 };
 
+// This solve a bug with clang and libc++
+// http://stackoverflow.com/q/29324619
+template class std::basic_string<char>;
 
 int main(int argc, char* argv[]) {
 	std::vector<int> vector = {1, 2, 3, 4, 5};
+    boost::array<std::string, 3> str_values = {"We ", "are", "the champions!"};
 
 	{
 		// simple use of binding function
 		// arbitrary function objects, functions, function pointers, and member function pointers
 		std::for_each(vector.begin(), vector.end(), boost::bind(print_number, "Value: ", _1));
-	}
+
+		std::size_t count = std::count_if(vector.begin(), vector.end(), boost::bind(std::less<int>(), 2, _1));
+        count = std::count_if(str_values.begin(), str_values.end(), boost::bind(&std::string::empty, _1));
+    }
 
 	{
 		// function composition
@@ -45,15 +54,15 @@ int main(int argc, char* argv[]) {
 	}
 
 	{
-		// logical operators
+		// operators
 		std::copy_if(vector.begin(), vector.end(),
 					 std::ostream_iterator<int>(std::cout, " "),
 					 !boost::bind<bool>([](int n)->bool{ return n % 2 == 0; }, _1));
 
-	}
 
-
-
+        std::size_t count = std::count_if(str_values.begin(), str_values.end(),
+                                          boost::bind(&std::string::size, _1) < 5);
+    }
 
 	return EXIT_SUCCESS;
 }
